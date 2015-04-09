c72f2caa90c82cf10750931452622869dd108eb8
Matt Kereczman <matt@linbit.com>
2015-04-09 15:05:40 +0200

    drbd-devstack: Initial implementation.
    This devstack plugin helps to install DRBD on Cinder nodes;
    this provides a distributed, replicated, high-performant
    block storage.
    
    Change-Id: Ibd1a81c1af66a7aac14bed7840a918684a5a40ca

---
+++ drbd-devstack/devstack/plugin.sh
@@ -0,0 +1,41 @@
+# check for service enabled
+if is_service_enabled drbd_devstack; then
+    if [[ "$1" == "source" ]]; then
+        # Initial source of lib script
+        source $(dirname "$0")/lib/drbd_devstack
+    fi
+
+    if [[ "$1" == "stack" && "$2" == "pre-install" ]]; then
+        # Set up system services
+        echo_summary "Configuring system services drbd_devstack"
+        pre_install_drbd_devstack
+
+    elif [[ "$1" == "stack" && "$2" == "install" ]]; then
+        # Perform installation of service source
+        echo_summary "Installing drbd_devstack"
+        install_drbd_devstack
+
+    elif [[ "$1" == "stack" && "$2" == "post-config" ]]; then
+        # Configure after the other layer 1 and 2 services have been configured
+        echo_summary "Configuring drbd_devstack"
+        configure_drbd_devstack
+
+    elif [[ "$1" == "stack" && "$2" == "extra" ]]; then
+        # Initialize and start the drbd_devstack service
+        echo_summary "Initializing drbd_devstack"
+        init_drbd_devstack
+    fi
+
+    if [[ "$1" == "unstack" ]]; then
+        # Shut down drbd_devstack services
+        # no-op
+        shutdown_drbd_devstack
+    fi
+
+    if [[ "$1" == "clean" ]]; then
+        # Remove state and transient data
+        # Remember clean.sh first calls unstack.sh
+        # no-op
+        cleanup_drbd_devstack
+    fi
+fi
